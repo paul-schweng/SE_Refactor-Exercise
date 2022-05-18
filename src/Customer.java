fba5019ca98f414add3c281e31680930ee72ef66
Paul Schweng <paul.schweng@siemens.com>
2022-05-18 19:41:18 +0200

    structured code

--- src/Customer.java
+++ src/Customer.java
@@ -31,35 +31,44 @@ class Customer {
         while (enum_rentals.hasMoreElements()) {
             double thisAmount = 0;
             Rental each = (Rental) enum_rentals.nextElement();
+
             //determine amounts for each line
             thisAmount = amountFor(each);
+
             // add frequent renter points
-            frequentRenterPoints ++;
+            frequentRenterPoints++;
+
             // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
-                frequentRenterPoints ++;
+                frequentRenterPoints++;
+
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
         }
+
         //add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+
         return result;
     }
 
 
     private double amountFor(Rental each) {
         double thisAmount = 0;
+
         switch (each.getMovie().getPriceCode()) {
             case Movie.REGULAR:
                 thisAmount += 2;
                 if (each.getDaysRented() > 2)
                     thisAmount += (each.getDaysRented() - 2) * 1.5;
                 break;
+
             case Movie.NEW_RELEASE:
                 thisAmount += each.getDaysRented() * 3;
                 break;
+
             case Movie.CHILDRENS:
                 thisAmount += 1.5;
                 if (each.getDaysRented() > 3)
@@ -68,6 +77,7 @@ class Customer {
         }
         return thisAmount;
     }
+
 
 }
     
