be5f4980b4aed265410ca37173650bdb39ff31a6
Paul Schweng <paul.schweng@siemens.com>
2022-05-18 19:43:56 +0200

    moved amountFor method

--- src/Customer.java
+++ src/Customer.java
@@ -33,7 +33,7 @@ class Customer {
             Rental each = (Rental) enum_rentals.nextElement();
 
             //determine amounts for each line
-            thisAmount = amountFor(each);
+            thisAmount = each.amountFor();
 
             // add frequent renter points
             frequentRenterPoints++;
@@ -55,28 +55,6 @@ class Customer {
     }
 
 
-    private double amountFor(Rental each) {
-        double thisAmount = 0;
-
-        switch (each.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
-                break;
-
-            case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
-                break;
-
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
-    }
 
 
 }
