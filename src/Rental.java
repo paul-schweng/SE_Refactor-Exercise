e9dd230e68090429e8913214c589b599e05294e4
Paul Schweng <paul.schweng@siemens.com>
2022-05-18 20:07:19 +0200

    changed to newer switch statement

--- src/Rental.java
+++ src/Rental.java
@@ -23,22 +23,21 @@ class Rental {
         double thisAmount = 0;
 
         switch (this.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
+            case Movie.REGULAR -> {
                 thisAmount += 2;
                 if (this.getDaysRented() > 2)
                     thisAmount += (this.getDaysRented() - 2) * 1.5;
-                break;
+            }
 
-            case Movie.NEW_RELEASE:
-                thisAmount += this.getDaysRented() * 3;
-                break;
+            case Movie.NEW_RELEASE -> thisAmount += this.getDaysRented() * 3;
 
-            case Movie.CHILDREN:
+            case Movie.CHILDREN -> {
                 thisAmount += 1.5;
                 if (this.getDaysRented() > 3)
                     thisAmount += (this.getDaysRented() - 3) * 1.5;
-                break;
+            }
         }
+
         return thisAmount;
     }
 
